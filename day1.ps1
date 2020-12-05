de7df3a72d54a01c554e67d68136c4cca4b98277
Alvynskio <alvynb@pm.me>
2020-12-05 10:03:21 -0500

    finished day1, added day2input

--- day1.ps1
+++ day1.ps1
@@ -1,4 +1,10 @@
-foreach ($line in (Get-Content day1input.txt)) {
-    $sum += [int]$line
+$nums = (Get-Content day1input.txt).Split(' ')
+for ($n = 0; $n -lt $nums.Length; $n++) {
+    for ($n2 = $n; $n2 -lt $nums.Length; $n2++) {
+        for ($n3 = $n2; $n3 -lt $nums.Length; $n3++) {
+            if ([int]$nums[$n] + [int]$nums[$n2] + [int]$nums[$n3] -eq 2020) {
+                Write-Host([int]$nums[$n] * [int]$nums[$n2] * [int]$nums[$n3])
+            }
+        }
+    }
 }
-Write-Host($sum)
